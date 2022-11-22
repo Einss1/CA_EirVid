211cb61a5319e214c16196eacae9d4970c47602f
Einss1 <luan_navarro@hotmail.com>
2022-11-22 16:37:39 +0000

    Commit 1
    Creation of the registration and login systems with connection to external database.

---
+++ EirVid/src/eirvid/EirVid.java
@@ -0,0 +1,50 @@
+/* Luan Navarro Silva - 2020344 */
+package eirvid;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EirVid {
+    
+    public static void main(String[] args) throws SQLException, NoSuchAlgorithmException, IOException {
+        System.out.println("Welcome to ÉirVid!\nPlease select an option:" );
+        int input;
+        
+        /* Switch menu */
+        do {
+            System.out.println("1 - Login \n2 - Register \n0 - Exit");
+            Scanner scanner = new Scanner(System.in);
+            while(!scanner.hasNextInt()){
+                System.out.println("Input is not a number! Please pick a number!");
+                scanner.nextLine();
+            }
+            input = scanner.nextInt();
+            scanner.nextLine();
+            switch (input){
+                
+                // Login
+                case 1:
+                    Login lo = new Login();
+                    lo.Login();
+                    break;
+                
+                // Registration
+                case 2:
+                    Registration re = new Registration();
+                    re.Registration();               
+                    break;
+                
+                // Exit
+                case 0:
+                    System.out.println("Thank you for using ÉirVid!");
+                    System.exit(0);
+                    
+                default:
+                    System.out.println("That is not a valid option! Please pick again!");
+                    break;
+            }   
+        } while (input != 0);  
+    }
+}
---
+++ EirVid/src/eirvid/Login.java
@@ -0,0 +1,49 @@
+/* Luan Navarro Silva - 2020344 */
+package eirvid;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Login {
+    public void Login() throws SQLException, NoSuchAlgorithmException, IOException {
+        /* Collect user account information */
+        System.out.println("Please insert your email address!");
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.nextLine().trim();
+        System.out.println("Please insert your password!");
+        String password = scanner.nextLine().trim();
+        
+        /* Create a MD5 hash to compare with the one stored in database and grant access */
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] messageDigest = md.digest(password.getBytes());
+        BigInteger bigInt = new BigInteger(1, messageDigest);
+        password = bigInt.toString();
+        
+        PreparedStatement st;
+        ResultSet rs;
+              
+        String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+                            
+        st = My_CNX.getConnection().prepareStatement(query);
+                
+        st.setString(1, email);
+        st.setString(2, password);
+        rs = st.executeQuery();
+                
+        if(rs.next()){
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            MainMenu mm = new MainMenu();
+            mm.MainMenu(id, name);
+        }
+        else {
+            System.out.println("Password incorrect or account not found!");
+        }
+    }
+}
