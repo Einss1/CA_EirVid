917013d3ceac68e4b7d326bee96809740bd59ef4
Awa <awa2025@gmail.com>
2022-11-30 12:41:43 +0000

    Update MainMenu.java
    Add and modify some code

--- EirVid/src/eirvid/MainMenu.java
+++ EirVid/src/eirvid/MainMenu.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 import java.util.Scanner;
 
 public class MainMenu {
@@ -16,7 +17,7 @@ public class MainMenu {
         
         /* Switch menu */
         do {
-            System.out.println("\n1 - Rent movie \n0 - Exit");
+            System.out.println("\n1 - Rent movie \n2 - Recommended \n0 - Exit");
             Scanner scanner = new Scanner(System.in);
             while(!scanner.hasNextInt()){
                 System.out.println("Input is not a number! Please pick a number!");
@@ -28,7 +29,35 @@ public class MainMenu {
                 
                 // Rent movie
                 case 1:
-                    try { 
+                    long timeNow = Instant.now().toEpochMilli();
+                    boolean canRent = true;
+                    /* Get information from database to check if user has a movie to be returned, if yes, the user can't rent another */
+                    try {
+                        PreparedStatement st;
+                        ResultSet rs;
+                        
+                        String query = "SELECT * FROM users WHERE id = ?";
+                        st = My_CNX.getConnection().prepareStatement(query);
+                        
+                        String stringId = Integer.toString(id);
+                        st.setString(1, stringId);
+                        
+                        rs = st.executeQuery();
+                        
+                        while(rs.next()) {
+                            long returnTime = rs.getLong("returnTime");
+                            if(returnTime > timeNow) {
+                                canRent = false;
+                                break;
+                            }
+                        }                                    
+                        
+                    } catch ( SQLException err) {
+                        System.out.println("Something went wrong while fetching information from users database");
+                    }
+                    
+                    if (canRent != false) {
+                                           try { 
                         PreparedStatement st;
                         ResultSet rs;
                         
@@ -69,16 +98,26 @@ public class MainMenu {
                         rs = st.executeQuery();
                 
                         if(rs.next()){
+                            int movieId = rs.getInt("id");
                             String movie = rs.getString("movie");
                             int price = rs.getInt("price");
                             System.out.println(name + ", you rented " + movie + " for " + price + " €");
+                            RentMovie.RentMovie(id, movieId);
                         } else {
                             System.out.println("Something went wrong while fetching movie from database. Please try again!");
                         }
                     } catch (SQLException err) {
                         System.out.println("Something went wrong while renting a movie. Please try again!");
                     }
+                    } else {
+                        System.out.println("Sorry, you can't rent at the moment");
+                    }
                     break; 
+                
+                //Check recommended
+                case 2:
+                    Recommended.Recommended();
+                    break;
                     
                 // Exit
                 case 0:
