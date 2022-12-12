b57635d34ef7e4cd3d3fd1fecf544a0473e5f3b6
Awa <awa2025@gmail.com>
2022-12-12 16:37:21 +0000

    Modify and testing
    Sets SQL max connections to 5000

--- EirVid/src/eirvid/AdminPage.java
+++ EirVid/src/eirvid/AdminPage.java
@@ -7,47 +7,55 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AdminPage {
+
     public void AdminPage(int id) throws SQLException, NoSuchAlgorithmException, IOException {
         int input;
-        
+
         /* Switch menu */
         do {
             System.out.println("1 - Update database \n9 - Logout \n0 - Exit");
             Scanner scanner = new Scanner(System.in);
-            while(!scanner.hasNextInt()){
+            while (!scanner.hasNextInt()) {
                 System.out.println("Input is not a number! Please pick a number!");
                 scanner.nextLine();
             }
             input = scanner.nextInt();
             scanner.nextLine();
-            switch (input){
-                
-                // Update database
+            switch (input) {
+
+                /* Update database option */
                 case 1:
                     PreparedStatement st;
-                    ResultSet rs;
-                    
-                    //Delete previous database if exists.
+
+                    /* Sets SQL max connections to 5000 */
+                    try {
+                        st = My_CNX.getConnection().prepareStatement("SET GLOBAL max_connections = 5000"); /* https://stackoverflow.com/questions/19822558/how-to-set-max-connections-in-mysql-programmatically */
+                        st.executeUpdate();
+                        System.out.println("Max connections set!");
+                    } catch (SQLException err) {
+                        System.out.println("Something went wrong while setting max connections");
+                    }
+
+                    /* Delete previous database if exists */
                     try {
                         st = My_CNX.getConnection().prepareStatement("DROP TABLE IF EXISTS  movies");
                         st.executeUpdate();
                         System.out.println("Previous database deleted successfully!");
-                    } catch(SQLException err) {
+                    } catch (SQLException err) {
                         System.out.println("Something went wrong while deleting previous database. Please try again!");
                     }
-                    
-                    //Create a new empty database.
+
+                    /* Create a new empty database */
                     try {
-                        st = My_CNX.getConnection().prepareStatement("CREATE TABLE  movies (\n" +
-                            "  `id` smallint(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,\n" +
-                            "  `movie` varchar(255) NOT NULL,\n" +
-                            "  `price` smallint(255) NOT NULL\n" +
-                        ")" );
+                        st = My_CNX.getConnection().prepareStatement("CREATE TABLE  movies (\n"
+                                + "  `id` smallint(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,\n"
+                                + "  `movie` varchar(255) NOT NULL,\n"
+                                + "  `price` smallint(255) NOT NULL\n"
+                                + ")");
                         st.executeUpdate();
                         System.out.println("New database created successfully!");
                     } catch (SQLException err) {
@@ -56,55 +64,54 @@ public class AdminPage {
 
                     String line = "";
                     String splitBy = ",";
-                    int i = 0;             
+                    int i = 0;
                     int[] movieId = new int[50000];
                     String[] movieTitle = new String[50000];
                     int[] moviePrice = new int[50000];
-                    
-                    //Insert into new database movies and its price.
+
+                    /* Insert into new database movies and their prices. */
                     try {
                         BufferedReader br = new BufferedReader(new FileReader("src/eirvid/movies.csv"));
                         while ((line = br.readLine()) != null) {
                             String[] movie = line.split(splitBy);
                             movieId[i] = i;
                             movieTitle[i] = movie[0];
-                            String numberOnly= movie[1].replaceAll("[^0-9]", "");
+                            String numberOnly = movie[1].replaceAll("[^0-9]", "");
                             int price = Integer.parseInt(numberOnly);
                             moviePrice[i] = price;
-        
+
                             String queryInsert = "INSERT INTO movies(movie, price) VALUES (?, ?)";
-        
+
                             st = My_CNX.getConnection().prepareStatement(queryInsert);
                             st.setString(1, movieTitle[i]);
                             String stringPrice = Integer.toString(moviePrice[i]);
                             st.setString(2, stringPrice);
                             i++;
-                            if(st.executeUpdate() != 0) {
-                                System.out.println("Movie " + movieTitle[i-1] + " was inserted into database successfuly!");
-                            }else{
+                            if (st.executeUpdate() != 0) {
+                                System.out.println("Movie " + movieTitle[i - 1] + " was inserted into database successfuly!");
+                            } else {
                                 System.out.println("Something went wrong, please try again!");
-                            } 
+                            }
                         }
-                    }
-                    catch(IOException e) {
+                    } catch (IOException e) {
                         System.out.println("Something went wrong while inserting data into database!");
                     }
                     break;
-                    
-                //Logout
+
+                /* Logout option */
                 case 9:
-                    main(null);  
+                    main(null);
                     break;
 
-                // Exit
+                /* Exit option */
                 case 0:
                     System.out.println("Thank you for using ÉirVid!");
                     System.exit(0);
-                    
+
                 default:
                     System.out.println("That is not a valid option! Please pick again!");
                     break;
-            }   
-        } while (input != 0);  
+            }
+        } while (input != 0);
     }
 }
--- EirVid/src/eirvid/MainMenu.java
+++ EirVid/src/eirvid/MainMenu.java
@@ -12,129 +12,132 @@ import java.time.Instant;
 import java.util.Scanner;
 
 public class MainMenu {
+
     public void MainMenu(int id, String name) throws FileNotFoundException, IOException, SQLException, NoSuchAlgorithmException {
-        
-        System.out.println("Please select an option:" );
+
+        System.out.println("Please select an option:");
         int input;
-        
+
         /* Switch menu */
         do {
             System.out.println("\n1 - Rent movie \n2 - Recommended \n9 - Logout \n0 - Exit");
             Scanner scanner = new Scanner(System.in);
-            while(!scanner.hasNextInt()){
+            while (!scanner.hasNextInt()) {
                 System.out.println("Input is not a number! Please pick a number!");
                 scanner.nextLine();
             }
             input = scanner.nextInt();
             scanner.nextLine();
-            switch (input){
-                
-                // Rent movie
+            switch (input) {
+
+                /* Rent movie option*/
                 case 1:
                     long timeNow = Instant.now().toEpochMilli();
                     boolean canRent = true;
+
                     /* Get information from database to check if user has a movie to be returned, if yes, the user can't rent another */
                     try {
                         PreparedStatement st;
                         ResultSet rs;
-                        
+
                         String query = "SELECT * FROM users WHERE id = ?";
                         st = My_CNX.getConnection().prepareStatement(query);
-                        
+
                         String stringId = Integer.toString(id);
                         st.setString(1, stringId);
-                        
+
                         rs = st.executeQuery();
-                        
-                        while(rs.next()) {
+
+                        while (rs.next()) {
                             long returnTime = rs.getLong("returnTime");
-                            if(returnTime > timeNow) {
+                            if (returnTime > timeNow) {
                                 canRent = false;
                                 break;
                             }
-                        }                                    
-                        
-                    } catch ( SQLException err) {
+                        }
+
+                    } catch (SQLException err) {
                         System.out.println("Something went wrong while fetching information from users database");
                     }
-                    
+
+                    /* Get all movies from the database and display to the user */
                     if (canRent != false) {
-                                           try { 
-                        PreparedStatement st;
-                        ResultSet rs;
-                        
-                        String query = "SELECT * FROM movies";
-                            
-                        st = My_CNX.getConnection().prepareStatement(query);
-                
-                        rs = st.executeQuery();
-                        System.out.println("ID" + "\t" + "MOVIE" + "\t\t" + "PRICE\n");
-                        while(rs.next()) {
-                            int movieId = rs.getInt("id");
-                            String movie = rs.getString("movie");
-                            int price = rs.getInt("price");
-                            System.out.println(movieId + "\t" + movie + "\t" + price + "€");
+                        try {
+                            PreparedStatement st;
+                            ResultSet rs;
+
+                            String query = "SELECT * FROM movies";
+
+                            st = My_CNX.getConnection().prepareStatement(query);
+
+                            rs = st.executeQuery();
+                            System.out.println("ID" + "\t" + "MOVIE" + "\t\t" + "PRICE\n");
+                            while (rs.next()) {
+                                int movieId = rs.getInt("id");
+                                String movie = rs.getString("movie");
+                                int price = rs.getInt("price");
+                                System.out.println(movieId + "\t" + movie + "\t" + price + "€");
+                            }
+
+                        } catch (SQLException err) {
+                            System.out.println("Something went wrong while fetching movies from database. Please try again!");
                         }
-                        
-                    } catch ( SQLException err) {
-                        System.out.println("Something went wrong while fetching movies from database. Please try again!");
-                    }
-                    
-                    System.out.println("\nPick a movie from the list to rent!");
-                    while(!scanner.hasNextInt()){
-                        System.out.println("Input is not a number! Please pick a number!");
+
+                        /* Rent the movie picked by user */
+                        System.out.println("\nPick a movie from the list to rent!");
+                        while (!scanner.hasNextInt()) {
+                            System.out.println("Input is not a number! Please pick a number!");
+                            scanner.nextLine();
+                        }
+                        input = scanner.nextInt();
                         scanner.nextLine();
-                    }
-                    input = scanner.nextInt();
-                    scanner.nextLine();
-                    try {
-                        PreparedStatement st;
-                        ResultSet rs;
-                        
-                        String query = "SELECT * FROM movies WHERE id = ?";
-                        
-                        st = My_CNX.getConnection().prepareStatement(query);
-                        
-                        String stringInput = Integer.toString(input);
-                        st.setString(1, stringInput);
-                        rs = st.executeQuery();
-                
-                        if(rs.next()){
-                            int movieId = rs.getInt("id");
-                            String movie = rs.getString("movie");
-                            int price = rs.getInt("price");
-                            System.out.println(name + ", you rented " + movie + " for " + price + " €");
-                            RentMovie.RentMovie(id, movieId);
-                        } else {
-                            System.out.println("Something went wrong while fetching movie from database. Please try again!");
+                        try {
+                            PreparedStatement st;
+                            ResultSet rs;
+
+                            String query = "SELECT * FROM movies WHERE id = ?";
+
+                            st = My_CNX.getConnection().prepareStatement(query);
+
+                            String stringInput = Integer.toString(input);
+                            st.setString(1, stringInput);
+                            rs = st.executeQuery();
+
+                            if (rs.next()) {
+                                int movieId = rs.getInt("id");
+                                String movie = rs.getString("movie");
+                                int price = rs.getInt("price");
+                                RentMovie.RentMovie(name, id, movieId, movie, price);
+                            } else {
+                                System.out.println("Something went wrong while fetching movie from database. Please try again!");
+                            }
+                        } catch (SQLException err) {
+                            System.out.println("Something went wrong while renting a movie. Please try again!");
                         }
-                    } catch (SQLException err) {
-                        System.out.println("Something went wrong while renting a movie. Please try again!");
-                    }
                     } else {
                         System.out.println("Sorry, you can't rent at the moment");
                     }
-                    break; 
-                
-                //Check recommended
+                    break;
+
+                /* Recommended movies option */
                 case 2:
                     Recommended.Recommended();
                     break;
-                
-                //Logout
+
+                /* Logout option */
                 case 9:
-                    main(null);  
+                    main(null);
                     break;
-                    
-                // Exit
+
+                /* Exit option */
                 case 0:
                     System.out.println("Thank you for using ÉirVid!");
                     System.exit(0);
-                    
+
                 default:
                     System.out.println("That is not a valid option! Please pick again!");
                     break;
-            }   
+            }
         } while (input != 0);
     }
 }
